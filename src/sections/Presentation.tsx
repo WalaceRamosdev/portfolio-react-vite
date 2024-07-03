664df208ad8750b635c5ed2349f972ad433a983a
Walace Ramos <contatowalace.dev@gmail.com>
2024-07-03 18:54:16 -0300

    deploy

--- src/sections/Presentation.tsx
+++ src/sections/Presentation.tsx
@@ -1,45 +1,50 @@
 'use client'
 
-import { gsap } from 'gsap'
-import { useEffect, useRef } from 'react'
+import { useEffect, useState } from 'react'
 
 import style from './css/Presentation.module.css'
 import { Button } from '../components/buttons/Button'
 
 export const Presentation = () => {
 
-    const tl = gsap.timeline({ defaults: { duration: 0.75 } })
-
-    const titleRef =  useRef(null)
-    const descriptionRef = useRef(null)
-
+        const [opacity, setOpacity] = useState(0);
+        const message = 'Com uma longa experiência no desenvolvimento de websites e aplicações web, estou aqui para transformar suas ideias em realidade digital. Especializado em tecnologias modernas como HTML5, CSS3, JavaScript, ReactJs, e Node.js, ofereço soluções sob medida que atendem às necessidades específicas do seu negócio.';
+    
     useEffect(() => {
-        const title = titleRef.current
-        const description = descriptionRef.current
+        let currentOpacity = 0;
+            const intervalId = setInterval(() => {
+                currentOpacity += 0.1;
+                setOpacity(currentOpacity);
+        if (currentOpacity >= 1) {
+            clearInterval(intervalId);
+        }
+    }, 100);
 
-    tl.fromTo(title, {
-            opacity:0, x: 100},
-        {   opacity: 1, x: 0 },)
-
-    .fromTo(description, {
-            opacity:0, y: 50},
-        {  opacity: 1, y: 0,}, "-=0.57"
-    )
-    }, [])
+        return () => clearInterval(intervalId);
+}, []);
 
     return (
+
+        <div
+        style={{
+        opacity: opacity,
+        transition: 'opacity 1s',
+        fontSize: '24px',
+        fontFamily: 'Arial, sans-serif'
+    }}
+    >
         <main className={style.main}>
             
-            <h1 ref={titleRef}>Olá! Sou Walace Ramos</h1>
+            <h1>Olá! Sou Walace Ramos</h1>
 
-            <p ref={descriptionRef}>
-                Com uma longa experiência no desenvolvimento de websites e aplicações web, estou aqui 
-                para transformar suas ideias em realidade digital. Especializado em tecnologias modernas 
-                como HTML5, CSS3, JavaScript, ReactJs, e Node.js, ofereço soluções sob medida que 
-                atendem às necessidades específicas do seu negócio.</p>
+            <p>
+                {message}
+            </p>
 
                 <Button text='Meu Instagram' link="https://www.instagram.com/walacedev.contato/"/>
 
         </main>
+
+        </div>
     )
 }
